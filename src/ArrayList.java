baaf4c8ed1ff7c5af47ff4c49bceec99e1f3b4c3
Michael Diep <michaeldiep@infra04-wg012.eduroam.wireless.umn.edu>
2023-03-24 13:10:35 -0500

    Hey

---
+++ src/ArrayList.java
@@ -0,0 +1,7 @@
+public class ArrayList<T extends Comparable<T>> implements List<T> {
+    T[] a = (T[]) new Comparable[size];
+    private boolean isSorted;
+    public ArrayList(int initsize){
+        int size = 2;
+    }
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,6 @@
+public class LinkedList<T extends Comparable<T>> implements List<T> {
+    private boolean isSorted;
+    public LinkedList(){
+
+    }
+}
