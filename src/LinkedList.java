552c33bae7441ed73056d73adb205da9e62ba48b
Anna Breck <annallucille@Annas-MacBook-Air-73.local>
2023-03-27 17:21:28 -0500

    again

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,6 +1,44 @@
+import java.util.List;
+
+
 public class LinkedList<T extends Comparable<T>> implements List<T> {
     private boolean isSorted;
-    public LinkedList(){
+    private Node start;
+    private List list;
+    private Node currNode = start.getNext();
 
+    public LinkedList(){
+        this.start = new Node("\0");
+        isSorted = false;
     }
+
+    public void add(Node node){
+        if(this.start.getNext()==null){
+            this.start.setNext(node);
+        }
+
+        while(currNode.getNext()!=null){
+            currNode = currNode.getNext();
+        }
+        currNode.setNext(node);
+    }
+
+    public boolean sort(){
+        while(currNode.getNext()!=null){
+            Node nextNode = currNode.getNext();
+            Node temp;
+            if(currNode.getData().compareTo(nextNode.getData())==1){
+                temp = nextNode;
+                nextNode.setNext(currNode);
+                currNode = temp.getNext();
+            }
+            else{
+                currNode = nextNode;
+            }
+        }
+        isSorted = true;
+        return isSorted;
+    }
+
+
 }
