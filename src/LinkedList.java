6dca8265597737206f0533e64dc508a31888d99d
Anna Lucille Breck <100645252+annallucille@users.noreply.github.com>
2023-03-29 13:41:45 -0500

    three pass

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,48 +1,230 @@
-import java.util.List;
-
-
 public class LinkedList<T extends Comparable<T>> implements List<T> {
+    private boolean isSorted = true;
+    private Node<T> start;
+    private int size;
+    private Node<T> currNode;
 
-    private boolean isSorted;
-    private Node start;
-    private List list;
-    private Node currNode = start.getNext();
 
 
     public LinkedList(){
-        this.start = new Node("\0");
+        this.start = new Node(null);
         isSorted = false;
+        size = 0;
     }
 
-    public void add(Node node){
-        if(this.start.getNext()==null){
-            this.start.setNext(node);
-        }
+    public boolean isSorted(){ return isSorted; }
 
+    public int size(){ return size; }
+
+    public boolean add(T data){
+        if(data == null){
+            return false;
+        }
+        Node<T> node = new Node(data);
+        if(this.start.getNext() == null){
+            this.start.setNext(node);
+            size += 1;
+            return true;
+        }
+        currNode = start.getNext();
+        if(currNode == null){
+            return false;
+        }
         while(currNode.getNext()!=null){
             currNode = currNode.getNext();
         }
         currNode.setNext(node);
+        size += 1;
+
+        return true;
+    }
+
+    public boolean add(int i, T data){
+        if(data == null){ return false; }
+        if(i > size + 1){ return false;}
+        if(this.isEmpty()){return false;}
+        currNode = start.getNext();
+        Node<T> node = new Node<>(data);
+        int j;
+        for(j = 0; j < i-1; j++){
+            currNode = currNode.getNext(); 
+        }
+        if(j == i-1) {
+            node.setNext(currNode.getNext());
+            currNode.setNext(node);
+            size += 1;
+            return true;
+        }
+        return false;
+    }
+
+    public int indexOf(T element){
+        currNode = start.getNext();
+        if(this.isEmpty()){ return -1; }
+        if(element == null){ return -1; }
+        for(int i = 0; i <= size; i++){
+            if(currNode == null){ return -1;}
+            else if (currNode.getData() == element){ return i;}
+            currNode = currNode.getNext();
+        }
+        return -1;
     }
 
 
-    public boolean sort(){
-        while(currNode.getNext()!=null){
-            Node nextNode = currNode.getNext();
-            Node temp;
-            if(currNode.getData().compareTo(nextNode.getData())==1){
-                temp = nextNode;
-                nextNode.setNext(currNode);
-                currNode = temp.getNext();
+    public T get(int i){
+        if(this.isEmpty()){ return null;}
+        if(i > size){return null; }
+        currNode = start.getNext();
+        for(int j = 0; j < i; j++){
+            currNode = currNode.getNext();
+        }
+        if(currNode==null){ return null;}
+        return currNode.getData();
+    } 
+
+
+    public void equalTo(T element){
+        int i = 0;
+        while(currNode.getNext()!=null) {
+            if (currNode.getData() != element){
+                currNode = currNode.getNext();
+                this.remove(i);
+                i++;
             }
             else{
-                currNode = nextNode;
+                currNode = currNode.getNext();
+                i++;
             }
+
         }
-        isSorted = true;
-        return isSorted;
     }
 
+    public void merge(List<T> otherList){
+        LinkedList<T> other = (LinkedList<T>) otherList;
+        other.sort();
+        if(!this.isSorted){
+            this.sort();
+        }
+        while(other.currNode != null){
+            while(this.currNode != null){
+                if(this.currNode.equals(other.currNode)){
+                    this.currNode = this.currNode.getNext();
+                    other.currNode = other.currNode.getNext();
+                }
+            this.currNode = this.currNode.getNext();
+            }
+            this.add(other.currNode.getData());
+            other.currNode = other.currNode.getNext();
+        }
+        this.sort();
+    }
+
+    public boolean rotate(int n){
+        currNode = start.getNext();
+        if(this.isEmpty()){
+            return false;
+        }
+        Node<T> temp = start.getNext();
+        for(int i = 0; i < size-n; i++){
+            currNode = currNode.getNext();
+        }
+        for(int j = (size - n); j == size; j++){
+            if(j == (size - n)){
+                start.setNext(currNode);
+            }
+            else if(currNode.getNext() == null){
+                currNode.setNext(temp);
+            }
+            else{
+                currNode.setNext(currNode.getNext());
+            }
+        }
+        return true;
+    }
+
+    public String toString(){
+        currNode = start.getNext();
+        String s = "";
+        while(currNode != null){
+            s += currNode.getData() + "\n";
+            if(currNode.getNext() == null){
+                return s;
+            }
+            currNode = currNode.getNext();
+        }
+        return s;
+    }
+
+    public T[] toArray(T[] array){
+        currNode = start.getNext();
+        int i = 0;
+        while(currNode != null){
+            array[i] = currNode.getData();
+            currNode = currNode.getNext();
+            i++;
+        }
+        return array;
+    }
+
+    public boolean isEmpty(){
+        return start.getNext() == null;
+    }
+
+    public void sort(){
+        if(this.isEmpty()){ return; }
+        currNode = start.getNext();
+        Node<T> nextNode;
+        while(currNode.getNext() != null){
+            if(currNode==null){ return;}
+            nextNode = currNode.getNext();
+            while(currNode.getData().compareTo(nextNode.getData()) > 0){
+                if(nextNode.getNext()==null){
+                    currNode.setNext(null);
+                    nextNode.setNext(currNode);
+                    currNode = nextNode;
+                    return;
+                }
+                currNode.setNext(nextNode.getNext());
+                nextNode.setNext(currNode);
+                currNode = nextNode;
+            }
+            currNode = nextNode;
+        }
+        isSorted = true;
+    }
+
+    public void reverse(){
+        Node<T> nextNode;
+        while(currNode.getNext() != null){
+            nextNode = currNode.getNext();
+            currNode.setNext(nextNode.getNext());
+            nextNode.setNext(currNode);
+            currNode = nextNode;
+        } 
+        start.setNext(currNode);
+    }
+
+    public void clear(){
+        currNode = start.getNext();
+        Node<T> temp;
+        if(currNode == null){
+            return;
+        }
+        while(currNode.getNext()!=null){
+            currNode.setData(null);
+            currNode = currNode.getNext();
+        }
+    }
+
+    public T remove(int i){
+        for(int j = 0; j < i-1; j++){
+            currNode = currNode.getNext();
+        }
+        Node<T> temp = currNode.getNext();
+        currNode.setNext(temp.getNext());
+        return temp.getData();
+    }
 
 
 }
+
